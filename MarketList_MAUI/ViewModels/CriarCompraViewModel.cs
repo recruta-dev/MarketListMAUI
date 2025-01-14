479d853f96cce39831ab692c558892d5480247b1
igorbdaluz <igorbdaluz@hotmail.com>
2025-01-14 01:29:46 -0300

    Organizando o layout de criar nova compra.

--- MarketList_MAUI/ViewModels/CriarCompraViewModel.cs
+++ MarketList_MAUI/ViewModels/CriarCompraViewModel.cs
@@ -2,5 +2,26 @@
 
 public class CriarCompraViewModel : ViewModelBase<Compra, Item>
 {
+	private string? _novoProduto;
+	public string? NovoProduto
+	{
+		get => _novoProduto;
+		set
+		{
+			_novoProduto = value;
+			OnPropertyChanged(nameof(NovoProduto));
+		}
+	}
+
+
 	public CriarCompraViewModel() { }
+
+	private void Adicionar(string produto)
+	{
+		ItemCollection!.Add(new Item { Nome = produto });
+		NovoProduto = string.Empty;
+		OnPropertyChanged(nameof(ItemCollection));
+	}
+
+	public ICommand AdicionarCommand => new Command<string>(Adicionar);
 }
