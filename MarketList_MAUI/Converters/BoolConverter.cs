cbaa4f23d92931d0df3a2d5e168b467601dd51f7
igorbdaluz <igorbdaluz@hotmail.com>
2025-01-16 23:41:25 -0300

    Fazendo o comportamento do campo de nome do mercado habilitado e desabilitado na página de nova compra.

---
+++ MarketList_MAUI/Converters/BoolConverter.cs
@@ -0,0 +1,15 @@
+﻿namespace MarketList_MAUI.Converters;
+
+public class BoolConverter : IValueConverter
+{
+    public object? Convert(object? value, Type targetType, object? parameter, CultureInfo culture)
+    {
+        if (value is bool boolean)
+            return boolean is true ? false : true;
+
+        return value;
+    }
+
+    public object? ConvertBack(object? value, Type targetType, object? parameter, CultureInfo culture)
+        => throw new NotImplementedException();
+}
--- MarketList_MAUI/ViewModels/CriarCompraViewModel.cs
+++ MarketList_MAUI/ViewModels/CriarCompraViewModel.cs
@@ -2,8 +2,8 @@
 
 public class CriarCompraViewModel : ViewModelBase<Compra, Item>
 {
-	private Item _produto;
-	public Item Produto
+	private Item? _produto;
+	public Item? Produto
 	{
 		get => _produto;
 		set
@@ -12,18 +12,37 @@ public class CriarCompraViewModel : ViewModelBase<Compra, Item>
 			OnPropertyChanged(nameof(Produto));
 		}
 	}
+	private bool _mercadoPreenchido;
+	public bool MercadoPreenchido
+	{
+		get => _mercadoPreenchido;
+		set 
+		{
+			_mercadoPreenchido = value;
+			OnPropertyChanged(nameof(MercadoPreenchido));
+		}
+	}
 
 	public CriarCompraViewModel()
 	{
 		Produto = new Item();
+		MercadoPreenchido = false;
 	}
 
-	private void Adicionar(string produto)
+	private void Adicionar()
 	{
-		ItemCollection!.Add(Produto);
+		ItemCollection!.Add(Produto!);
 		Produto = new Item();
 		OnPropertyChanged(nameof(ItemCollection));
 	}
+	private void PreencherMercado()
+	{
+		if (string.IsNullOrWhiteSpace(CurrentItem!.Mercado))
+			MercadoPreenchido = false;
+		else if (CurrentItem.Mercado.Length > 3)
+			MercadoPreenchido = true;
+	}
 
-	public ICommand AdicionarCommand => new Command<string>(Adicionar);
+	public ICommand AdicionarCommand => new Command(Adicionar);
+	public ICommand PreencherMercadoCommand => new Command(PreencherMercado);
 }
